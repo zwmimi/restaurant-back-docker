fceb8c3c820d8b18075f133e07eee0a2f1ddff7f
zwmimi <zwmimi1212@gmail.com>
2022-11-21 15:22:45 +0900

    ormconfigファイルの追加

---
+++ ormconfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  autoLoadEntities: true,
+  entities: ['dist/entities/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  cli: {
+    entitiesDir: 'src/entities',
+    migrationsDir: 'src/migrations',
+  },
+};
