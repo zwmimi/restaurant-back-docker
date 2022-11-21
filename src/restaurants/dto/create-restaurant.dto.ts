2c640f58bcc0781c7f02f5cd0b0d66fb131f49cc
zwmimi <zwmimi1212@gmail.com>
2022-11-21 15:07:43 +0900

    classValidatorでバリデーション実装

--- src/restaurants/dto/create-restaurant.dto.ts
+++ src/restaurants/dto/create-restaurant.dto.ts
@@ -1,5 +1,16 @@
+import { IsNotEmpty, IsString, IsUrl, MaxLength } from 'class-validator';
+
 export class CreateRestaurantDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(30)
   name: string;
+
+  @IsNotEmpty()
+  @IsUrl()
   url: string;
+
+  @IsString()
+  @MaxLength(80)
   description: string;
 }
